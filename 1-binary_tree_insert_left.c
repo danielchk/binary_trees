4785a54fa569e929ffda0a2995fb957fad698c11
danielchk <hollowdaniel1@gmail.com>
2020-07-21 16:57:23 +0000

    fixing betty and 4

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,20 +10,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *leftnode = NULL;
-    leftnode = binary_tree_node(parent, value);
+	binary_tree_t *leftnode = NULL;
 
-    if (parent == NULL)
-        return (NULL);
+	leftnode = binary_tree_node(parent, value);
 
-    if (leftnode == NULL)
-        return (NULL)
+	if (parent == NULL)
+		return (NULL);
 
-    if (parent->left)
-    {
-        parent->left->parent = leftnode;
-        leftnode->left = parent->left;
-    }
-    parent->left = leftnode;
-    return (leftnode);
+	if (leftnode == NULL)
+		return (NULL);
+
+	if (parent->left)
+	{
+		parent->left->parent = leftnode;
+		leftnode->left = parent->left;
+	}
+	parent->left = leftnode;
+	return (leftnode);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,22 +2,25 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_node_2 - create right node
+ * binary_tree_insert_right - create right node
  * @parent: parent
  * @value: value
  * Return: pointer to the new node
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *rightnode = NULL;
-    rightnode = binary_tree_node(parent, value);
+	binary_tree_t *rightnode = NULL;
 
-    if (parent == NULL)
-        return (NULL);
+	rightnode = binary_tree_node(parent, value);
 
-    if (parent->right)
-    {
-        parent->right->parent = rightnode;
-        rightnode->right = parent->right;
-    }
+	if (parent == NULL)
+		return (NULL);
+
+	if (parent->right)
+	{
+		parent->right->parent = rightnode;
+		rightnode->right = parent->right;
+	}
+	parent->right = rightnode;
+	return (rightnode);
 }
