134609e40e12b6e11aed5d06dfa7bc8be20ffda8
danielchk <hollowdaniel1@gmail.com>
2020-07-21 16:27:46 +0000

    corrections in excercise 1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,10 +9,10 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode;
+    binary_tree_t *new_node;
 
-    newnode = malloc(sizeof(binary_tree_t));
-    if (newnode == NULL)
+    new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL)
         return (NULL);
 
     new_node->parent = parent;
