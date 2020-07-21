df375c1441c9f9f15061a860263fbe335e0f6d60
danielchk <hollowdaniel1@gmail.com>
2020-07-21 17:08:48 +0000

    fixing ubications in code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,11 +12,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *leftnode = NULL;
 
-	leftnode = binary_tree_node(parent, value);
 
 	if (parent == NULL)
 		return (NULL);
 
+	leftnode = binary_tree_node(parent, value);
 	if (leftnode == NULL)
 		return (NULL);
 
