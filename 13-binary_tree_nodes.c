befa90f7ede40fdfc1163ea3b3caf8b4d92c502c
danielchk <hollowdaniel1@gmail.com>
2020-07-23 16:50:10 +0000

    excercise 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_nodes - count nodes with childs
+  * @tree: root node
+  * Return: nodes
+  */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if ((tree->left) || (tree->right))
+		nodes = 1;
+
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+
+	return (nodes);
+}
